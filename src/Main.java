0c343428527728ea3cff62172023e2a38fca15bf
laurenw <laurenw33@nycstudents.net>
2025-01-29 13:08:26 -0500

    added updates to part 1 of project

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        // step 1: create a file object
         File f = new File("src/txt");
 
 
@@ -21,77 +21,71 @@ public class Main {
 
             String[] fileArray = fileData.split("\n");
 
-            String numbers;
             String names;
+            String numbers;
+            String[] combined;
             for (int i = 0; i < fileArray.length; i++) {
                 String stringNumbers = fileArray[i];
                 String[] x = stringNumbers.split("\\|");
                 String[] y = x[0].split(",");
+                combined = y;
                 System.out.println(Arrays.toString(y));
 
                 numbers = "";
                 names = "";
-                for (int p = 0; p < y.length; p++){
+                for (int p = 0; p < y.length; p++) {
                     String list = y[p];
-                    if (list.equals("1")){
-                        numbers += "1" + " ";
-                    }
-                    else if (list.equals("2")){
-                        numbers += "2" + " ";
-                    }
-                    else if (list.equals("3")){
-                        numbers += "3" + " ";
-                    }
-                    else if (list.equals("4")){
-                        numbers += "4" + " ";
-                    }
-                    else if (list.equals("5")){
-                        numbers += "5" + " ";
-                    }
-                    else if (list.equals("6")){
-                        numbers += "6" + " ";
-                    }
-                    else if (list.equals("7")){
-                        numbers += "7" + " ";
-                    }
-                    else if (list.equals("8")){
-                        numbers += "8" + " ";
-                    }
-                    else if (list.equals("9")){
-                        numbers += "9" + " ";
-                    }
-                    else if (list.equals("10")){
-                        numbers += "10" + " ";
+                    if (list.equals("1")) {
+                        numbers += "1 ";
+                    } else if (list.equals("2")) {
+                        numbers += "2 ";
+                    } else if (list.equals("3")) {
+                        numbers += "3 ";
+                    } else if (list.equals("4")) {
+                        numbers += "4 ";
+                    } else if (list.equals("5")) {
+                        numbers += "5 ";
+                    } else if (list.equals("6")) {
+                        numbers += "6 ";
+                    } else if (list.equals("7")) {
+                        numbers += "7 ";
+                    } else if (list.equals("8")) {
+                        numbers += "8 ";
+                    } else if (list.equals("9")) {
+                        numbers += "9 ";
+                    } else if (list.equals("10")) {
+                        numbers += "10 ";
                     }
                 }
                 for (int p = 0; p < y.length; p++) {
                     String list = y[p];
-                    if (list.equalsIgnoreCase("ace")){
+                    if (list.equalsIgnoreCase("ace")) {
                         names += "Ace ";
-                    }
-                    else if (list.equalsIgnoreCase("king")){
+                    } else if (list.equalsIgnoreCase("king")) {
                         names += "King ";
-                    }
-                    else if (list.equalsIgnoreCase("queen")){
+                    } else if (list.equalsIgnoreCase("queen")) {
                         names += "Queen ";
-                    }
-                    else if (list.equalsIgnoreCase("jack")){
+                    } else if (list.equalsIgnoreCase("jack")) {
                         names += "Jack ";
                     }
                 }
                 String[] num = numbers.split(" ");
                 String[] name = names.split(" ");
 
-                System.out.println(Arrays.toString(num));
-                System.out.println(Arrays.toString(name));
+//                System.out.println(Arrays.toString(num));
+//                System.out.println(Arrays.toString(name));
+
+                PlainPoker p = new PlainPoker(num, name, combined);
+                System.out.println(Arrays.toString(p.determineHandType()));
+                System.out.println(p.handType(p.determineHandType()));
             }
-        }
-        catch (FileNotFoundException fe) {
+        } catch (FileNotFoundException fe) {
             System.out.println("File was not found");
             System.exit(1);
         }
 
     }
+
 
 }
 
--- src/PlainPoker.java
+++ src/PlainPoker.java
@@ -1,13 +1,90 @@
 public class PlainPoker {
 
     private int bidValue;
-    private int cardNum;
-    private String cardString;
+    private int[] cardNum;
+    private String[] cardString;
+    private String[] combined;
 
-    public PlainPoker(int num, String s){
-        num = cardNum;
-        s = cardString;
+    public PlainPoker(String[] num, String[] s, String[] initial){
+        combined = initial;
+        int len = num.length;
+        cardNum = new int[len];
+        for (int i = 0; i < num.length - 1; i++) {
+            cardNum[i] = Integer.parseInt(num[i]);
+        }
+        cardString = s;
 
     }
+    public int[] determineHandType() {
+        int len = combined.length;
+        int[] counts = new int[len];
 
+        for (int x = 0; x < len; x++) {
+            String currentCard = combined[x];
+            int count = 1;
+
+            for (int i = x + 1; i < len; i++) {
+                if (combined[i].equals(currentCard)) {
+                    count++;
+                    counts[i] = -1;
+                }
+            }
+
+            if (counts[x] != -1) {
+                counts[x] = count;
+            }
+        }
+        return counts;
+    }
+    public String handType(int[] counts) {
+        int fiveOfKind = 0;
+        int fourOfKind = 0;
+        int fullHouse = 0;
+        int threeOfKind = 0;
+        int twoPair = 0;
+        int onePair = 0;
+        int highCard = 0;
+        int pairs = 0;
+
+        for (int count : counts) {
+            if (count == 5) {
+                fiveOfKind++;
+            } else if (count == 4) {
+                fourOfKind++;
+            } else if (count == 3) {
+                threeOfKind++;
+            } else if (count == 2) {
+                pairs++;
+            }
+        }
+
+        if (fiveOfKind == 1) {
+            fiveOfKind++;
+        }
+        if (fourOfKind == 1) {
+            fourOfKind++;
+        }
+        if (threeOfKind == 1 && pairs == 1) {
+            fullHouse++;
+        }
+        if (threeOfKind == 1) {
+            threeOfKind++;
+        }
+        if (pairs == 2) {
+            twoPair++;
+        }
+        if (pairs == 1) {
+            onePair++;
+        }
+        highCard++;
+
+        return "Number of five of a kind hands:" + fiveOfKind + "\n";
+        return "Number of full house hands:" + fullHouse + "\n";
+        return "Number of four of a kind hands: " + fourOfKind+ "\n";
+        return "Number of three of a kind hands: " + threeOfKind+ "\n";
+        return "Number of two pair hands: " + twoPair+ "\n";
+        return "Number of one pair hands: " + onePair+ "\n";
+        return "Number of high card hands: " + highCard + "\n";
+
+    }
 }
